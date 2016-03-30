6759092e7457f7049b4333fafcd874aaecad0756
David Nagli <davidnagli@gmail.com>
2016-03-30 19:14:38 -0400

    Removed demo from repository, since no demo is made yet

--- demo/demo.js
+++
@@ -1,8 +0,0 @@
-whenPageLoads = function(){
-    sprites.mySprite1 = new Sprite(100,20,"<button>test</button>");
-    sprites.mySprite2 = new Sprite(0,0,"sprite.gif");
-    sprites.mySprite2.goTo(50,50,100);
-    var newElement = document.createElement("p");
-    newElement.innerHTML = "hello";
-    sprites.mySprite1.element = newElement;
-}; 
